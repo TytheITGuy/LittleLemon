a0347ba31c246c2612fc3934f86335a30de277b6
Tyler Literski <tylerliterski@gmail.com>
2026-01-04 18:07:52 -0500

    Add Django REST Framework and user API

--- littlelemon/littlelemon/urls.py
+++ littlelemon/littlelemon/urls.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import routers
+from restaurant.views import UserViewSet
+
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('restaurant/', include('restaurant.urls')),
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
 
--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from .serializers import UserSerializer
+
+
+# Template view for the homepage
 def index(request):
     return render(request, 'index.html', {})
+
+
+# DRF ViewSet for users
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
