2662ee95b9ec47f5b69670d750231313e94c342d
Tyler Literski <tylerliterski@gmail.com>
2026-01-04 18:41:19 -0500

    Add Menu API with Django REST Framework

--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+
+    path('menu/', views.MenuItemsView.as_view(), name='menu-items'),
+    path('menu/<int:pk>/', views.SingleMenuItemView.as_view(), name='single-menu-item'),
 ]
+
 
--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
-from rest_framework import viewsets
+from rest_framework import viewsets, generics
 from rest_framework.permissions import IsAuthenticated
 
-from .serializers import UserSerializer
+from .models import Menu
+from .serializers import UserSerializer, MenuSerializer
 
 
 # Template view for the homepage
@@ -16,3 +17,14 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [IsAuthenticated]
+
+
+# Menu API views
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+
+class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
